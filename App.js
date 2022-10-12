bcec535923a799fa198761888bbfd2c77741214d
Yanelsy Rivera <yanelsy2907@gmail.com>
2022-10-12 09:26:10 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Button, Linking, SafeAreaView, Alert } from 'react-native';
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export const URL = "https://api.cloudpayments.com/";
+const headers = {
+  "Content-Type": "application/json",
+  "API-KEY": "5cc31beb901a2e00017617f8af9a9076ba7541969a5dfb79b17248d1",
+  tenantName: "CANNACARD",
+  DEVICE_OS: "ANDROID",
+  DEVICE_ID: "",
+  TIMEZONE: "America/Caracas",
+  IP_ADDRESS: "",
+};
+
+export default class App extends React.Component {
+  
+  state = {
+    redirectData: null,
+  };
+
+  render() {
+    return(
+      <SafeAreaView style={{ flex: 1, marginTop: 24 }}>
+        <Button
+          onPress={this._requestUrl}
+          title="Open Ribbit"
+        /> 
+        {this._maybeRenderRedirectData()}
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    )
+  };
+
+  _requestUrl = async() => {
+    //login
+    var details = {
+        'username': "blake@tigrepay.com",
+        'password': "X332332x",
+        'grant_type': 'password'
+    };
+    var formBody = [];
+    for (var property in details) {
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(details[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    const response = await fetch(URL + "auth/oauth/token", {
+      method: "POST",
+      headers: Object.assign({}, headers, {
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
+      body: formBody,
+    }).then(response => response.json())
+    const url = `https://api.cloudpayments-staging.com/test-ribbit-widget/index2.html?token=${response.access_token}&tenantName=CANNACARD`
+    this.setState({ redirectData: url });
+    console.log(url)
+  };
+
+
+  _maybeRenderRedirectData = () => {
+    if (!this.state.redirectData) {
+      console.log("nothing")
+      return;
+    }
+    return (
+    <WebView originWhitelist={['*']} 
+          source={{ uri: this.state.redirectData }} 
+          javaScriptCanOpenWindowsAutomatically={true} 
+          javaScriptEnabled={true}
+          onMessage={(event)=> {
+            console.log(JSON.parse(event.nativeEvent.data).type)
+            if(JSON.parse(event.nativeEvent.data).type === "ENROLLMENT_SUCCESS")
+              Alert.alert("Data",event.nativeEvent.data)
+          }}
+          onShouldStartLoadWithRequest={(navState)=>{
+            Linking.openURL(navState.url)
+            return false;
+          }}
+        />
+    );
+  };
+  // useEffect(() => {
+  //   (async () => {
+      
+  //     //login
+  //     var details = {
+  //         'username': "blake@tigrepay.com",
+  //         'password': "X332332x",
+  //         'grant_type': 'password'
+  //     };
+  //     var formBody = [];
+  //     for (var property in details) {
+  //         var encodedKey = encodeURIComponent(property);
+  //         var encodedValue = encodeURIComponent(details[property]);
+  //         formBody.push(encodedKey + "=" + encodedValue);
+  //     }
+  //     formBody = formBody.join("&");
+  //     const response = await fetch(URL + "auth/oauth/token", {
+  //       method: "POST",
+  //       headers: Object.assign({}, headers, {
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //       }),
+  //       body: formBody,
+  //     }).then(response => response.json())
+
+  //     console.log(`https://api.cloudpayments-staging.com/test-ribbit-widget/index2.html?token=${response.access_token}&tenantName=CANNACARD`)
+  //   })()
+  // }, [])
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
